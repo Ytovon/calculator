c392c8fb8d1695022d884fbe500fcf8c12c9fc5f
Ytovon <125310811+Ytovon@users.noreply.github.com>
2024-10-11 09:08:32 +0200

    add method added

--- kalkulacka/Kalkulacka.cs
+++ kalkulacka/Kalkulacka.cs
@@ -8,15 +8,20 @@ namespace kalkulacka
 {
     internal class Kalkulacka
     {
-        public int PrvniCislo { get; set; }
-        public int DruhéCislo { get; set; }
+        public int PrvniCislo { get; }
+        public int DruhéCislo { get; }
+
+        public int vysledek { get; set; }
 
         public Kalkulacka(int prvniCislo, int druheCislo) { 
             this.PrvniCislo = prvniCislo;
             this.DruhéCislo = druheCislo;
         }
 
-
+        public void Secti()
+        {
+            this.vysledek = this.PrvniCislo + this.DruhéCislo;
+        }
 
 
     }
